b313bcb9359dfd8327c751ac05caf22cb6b72176
ductran <ductran.tb@gmail.com>
2024-09-06 15:53:55 +0700

    Fix unit test

--- test/Juice.MultiTenant.Tests/MultiTenantDbContextTest.cs
+++ test/Juice.MultiTenant.Tests/MultiTenantDbContextTest.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading;
 using System.Threading.Tasks;
-using Finbuckle.MultiTenant;
-using Finbuckle.MultiTenant.Abstractions;
 using FluentAssertions;
 using Juice.EF.Extensions;
 using Juice.Extensions.DependencyInjection;
-using Juice.MultiTenant.Domain.AggregatesModel.TenantAggregate;
 using Juice.MultiTenant.Tests.Domain;
 using Juice.MultiTenant.Tests.Infrastructure;
 using Juice.Services;
 using Juice.XUnit;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,7 +15,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Xunit;
 using Xunit.Abstractions;
-using Finbuckle.MultiTenant.AspNetCore.Internal;
 
 namespace Juice.MultiTenant.Tests
 {
@@ -106,18 +97,7 @@ namespace Juice.MultiTenant.Tests
                     });
 
                     services
-                        .AddMultiTenant()
-                        .JuiceIntegration()
-                        .WithInMemoryStore(options =>
-                        {
-                            options.Tenants.Add(new TenantInfo { Id = "TenantA", Identifier = "tenant-A" });
-                            options.Tenants.Add(new TenantInfo { Id = "TenantB", Identifier = "tenant-B" });
-                        })
-                        .WithDelegateStrategy((context) =>
-                        {
-                            var id = DateTime.Now.Millisecond % 2 == 0 ? "tenant-A" : "tenant-B";
-                            return Task.FromResult<string?>(id);
-                        });
+                        .AddTestTenantRandom<TenantInfo>();
 
                     services.AddTenantContentDbContext(configuration, provider, "Contents");
 
@@ -126,9 +106,7 @@ namespace Juice.MultiTenant.Tests
             for (var i = 0; i < 3; i++)
             {
                 using var scope = host.Services.CreateScope();
-
-                HttpContext httpContext = new MyHttpContext(scope.ServiceProvider);
-                var next = new RequestDelegate(async httpContext =>
+                await scope.ServiceProvider.TenantInvokeAsync(async httpContext =>
                 {
                     var serviceProvider = httpContext.RequestServices;
                     var context = serviceProvider.GetRequiredService<TenantContentDbContext>();
@@ -160,40 +138,10 @@ namespace Juice.MultiTenant.Tests
                     modifiedTime.Should().NotBeNull();
                     modifiedTime.Should().BeAfter(addedContent.CreatedDate);
                 });
-                await (new MultiTenantMiddleware(next).Invoke(httpContext));
 
             }
 
         }
     }
 
-    internal class MyHttpContext : HttpContext
-    {
-        private readonly IServiceProvider _serviceProvider;
-
-        public MyHttpContext(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public override IFeatureCollection Features => throw new NotImplementedException();
-
-        public override HttpRequest Request => throw new NotImplementedException();
-
-        public override HttpResponse Response => throw new NotImplementedException();
-
-        public override ConnectionInfo Connection => throw new NotImplementedException();
-
-        public override WebSocketManager WebSockets => throw new NotImplementedException();
-
-        public override ClaimsPrincipal User { get; set; }
-        public override IDictionary<object, object?> Items { get; set; } = new Dictionary<object, object?>();
-        public override IServiceProvider RequestServices { get => _serviceProvider; set => throw new NotImplementedException(); }
-        public override CancellationToken RequestAborted { get; set; }
-        public override string TraceIdentifier { get; set; } = new DefaultStringIdGenerator().GenerateUniqueId();
-        public override ISession Session { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public override void Abort() { }
-
-    }
 }
